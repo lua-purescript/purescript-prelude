eebc3fa861bfb42940fa3031b9a68275c38c1412
Martin Zeller <mz.bremerhaven@gmail.com>
2016-04-04 17:05:19 +0200

    Set topChar to char(255) instead of char(65535)
    Lua does not support unicode

--- src/Data/Bounded.lua
+++ src/Data/Bounded.lua
@@ -5,7 +5,7 @@ local exports = {}
 exports.topInt = 2147483647
 exports.bottomInt = -2147483648
 
-exports.topChar = string.char(65535)
+exports.topChar = string.char(255)
 exports.bottomChar = string.char(0)
 
 return exports
